0ca21748aa2bc6a2ab98aeab0be608ad3f847809
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-03-16 15:49:00 -0400

    Adding styles of PokemonOptions

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>PokemonOptions</h1>
+  <div class="options-container">
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -8,6 +15,28 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+
+ul {
+  list-style-type: none;
+}
+
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+}
+
+li:hover {
+  background-color: rgba(0, 0, 0, 0.05);
+}
+
+.options-container {
+  display: flex;
+  justify-content: center;
+}
 
 </style>
